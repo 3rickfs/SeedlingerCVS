1463aea9a82f3aa7cca1939c455b75fbec7a8708
Erick Fiestas <efiestass@upao.edu.pe>
2024-05-22 23:20:48 -0500

    fix the detectors module

--- tests/test_seedlinger_cvs.py
+++ tests/test_seedlinger_cvs.py
@@ -6,8 +6,9 @@ import random
 import time
 
 SEEDLING_CLASSIFIER_PATH  = '/home/robot/seedlinger/SeedlingerCVS'
-detectors_yolov7_path = '/home/robot/seedlinger/SeedlingerCVS/seedling_classifier/seedlingnet/modules/detector'
+detectors_yolov7_path = '/home/robot/seedlinger/SeedlingerCVS/seedling_classifier/seedlingnet/modules'
 sys.path.append(SEEDLING_CLASSIFIER_PATH)
+sys.path.append(detectors_yolov7_path)
 
 import seedlinger_cvs 
 from seedling_classifier.seedlingnet.modules.detector.detectors.yolo7 import Yolo7
