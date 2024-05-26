d3c8a4c2351ab30978d896cdc7faad9b308b8678
Erick Fiestas <efiestass@upao.edu.pe>
2024-05-26 11:47:27 -0500

    fix prediction variable lists

--- seedlinger_cvs.py
+++ seedlinger_cvs.py
@@ -67,12 +67,16 @@ def get_type_of_seedling(hp, vp, vm):
         else:
             print("Seedling detected properly")
             print(f"vp: {vp}")
-            v_pred_mask = cv2.resize(
-                vp.mask*255,
-                (vm.shape[1], vm.shape[0]),
-                interpolation=cv2.INTER_LINEAR
-            )
-            h_pred_mask = hp.mask
+            for v in vp:
+                v_pred_mask = cv2.resize(
+                    v.mask*255,
+                    (vm.shape[1], vm.shape[0]),
+                    interpolation=cv2.INTER_LINEAR
+                )
+                break
+            for h in hp:
+                h_pred_mask = h.mask
+                break
             #load the model if necessary
             if linear == "":
                 linear = Classifier('linear',CLASSIFIER_WEIGHTS)
