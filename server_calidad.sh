b49fd64b323b52a82fcb2fdb13d0de7585eb7176
prel2014 <ermafiso@gmail.com>
2024-06-05 11:00:11 -0500

    change seedling image folder path

--- server_calidad.sh
+++ server_calidad.sh
@@ -4,6 +4,4 @@ source /home/robot/miniconda3/etc/profile.d/conda.sh
 cd /home/robot/seedlinger/SeedlingerCVS
 conda activate seedlinger
 
-script -c 'ls' session.txt
-
 sudo /home/robot/miniconda3/envs/seedlinger/bin/python server_im_calidad.py
